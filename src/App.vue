e5d9faa4883585bb2aa52e0d8f3c9ad2c50a9650
Abialbon Paul <dr.abialbon@gmail.com>
2017-08-12 21:15:02 -0400

    Basic Navbar

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <app-nav></app-nav>
+    </div>
+</template>
+
+<script>
+    import appNav from './components/appNav.vue';
+    export default {
+        components: {
+            appNav
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/appNav.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <nav>
+            <div class="brand">Purcube</div>
+            <div class="settings">
+                <img src="../assets/svg/settings.svg" alt="">
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    nav {
+        height: 70px;
+        background-color: purple;
+        padding: 8px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .brand {
+        color: white;
+        font-size: 2.0em;
+    }
+    .settings img {
+        height: 40px;
+        width: 40px;
+    }
+</style>
