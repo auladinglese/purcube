942adf080c4031ca7b6a2600798264beaca16cee
Abialbon Paul <dr.abialbon@gmail.com>
2017-08-13 10:50:03 -0400

    Navbar form

--- src/components/appNav.vue
+++ src/components/appNav.vue
@@ -2,6 +2,12 @@
     <div>
         <nav>
             <div class="brand">Purcube</div>
+            <div class="form">
+                <form action="">
+                    <input type="text" name="urlstr" id="urlstr" placeholder="https://">
+                    <div><img src="../assets/svg/download.svg" alt=""></div>
+                </form>
+            </div>
             <div class="settings">
                 <img src="../assets/svg/settings.svg" alt="">
             </div>
@@ -25,6 +31,27 @@
     .brand {
         color: white;
         font-size: 2.0em;
+    }
+    form {
+        display: flex;
+    }
+    .form form input[type="text"] {
+        min-width: 270px;
+        height: 35px;
+        border: none;
+        padding: 4px;
+        border-radius: 4px;
+        font-size: 1.0em;
+    }
+    .form form div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .form form img {
+        margin-left: 5px;
+        height: 25px;
+        width: 25px;
     }
     .settings img {
         height: 40px;
